6155ff8f5b0fb365e193e57e7b924821c4cfaccf
Vinicius4555 <lev83575@gmail.com>
2026-05-13 16:58:25 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,23 +1,60 @@
-import streamlit as st
+# import streamlit as st
+# import minha_funcao as ms
 
-st.title('Portfólio')
-st.caption('Sejam bem vindos(as) a minha exposição dos meus trabalhos.')
+# st.title('titulo')
 
-# --------------------------------------------------------------------------
+# ms.soma(10,20)
+# ms.soma(200,500)
 
-# --------------------------------------------------------------------------
 
-st.title('Nome:')
-st.caption('Meu nome é Vinicius dos Santos Oliveira Ribeiro.')
+# # estrutura de dados
+# # espaço na memória da máquina
 
-st.title('Idade:')
-st.caption('Atualmente em 2026, possuo 16 anos.')
 
-st.title('Curiosidades:')
-st.caption('Possuo pouquissímo tempo trabalhando diariamente com computadores, ceca de 2~3 meses. Sou um aspirante a escritor, escrevo fantasias que se divergem mais para o Cyberpunk/Steampunk.')
+# # variavel   =  10
+# # lista  =  [1,2,3]
+# # dicionario =  {'a':10, 'b':20}
+# # conjuntos =  {10,20,303,30}
+# # tupla = (10,20,30,30)
 
-st.image('images.jpg')
-st.caption('Atualmente, ainda sou uma folha em branco, mas não uma folha qualquer, uma folha com potencial enorme para se colorida, revelando magnificos detalhes, que seriam tão complexos que sequer uma alma viva poderia compreende-los.')
 
-st.title('Meu Github!')
-st.caption('https://github.com/Vinicius4555')
+
+# # estutra de fluxo de controle 
+# # palavra_reserva condição
+
+
+# # if 10 >2:
+# #     print('é maior')
+
+
+# # for i in range(10):
+# #     print(i)
+
+
+# # while 10 < 1:
+# #     print('faça')    
+
+
+# # try :
+
+
+# # funções 
+# # palavras ()
+# # cria -  def nome ():
+# # Ação 
+# # função são nativas da lingugame 
+
+
+# # print()
+# # len()
+# # input()
+# # sum()
+# # ....
+
+
+
+# # def nome():
+# #     print('teste')
+
+
+# # nome()
